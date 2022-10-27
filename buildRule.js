e6c81f2f5e9d1c84672c147f9d4d965f44949f25
Manuel Bauer <manuel.bauer@dechea.com>
2022-10-27 05:58:49 +0200

    updated to create maps from json

--- buildRule.js
+++ buildRule.js
@@ -1,14 +1,28 @@
+const flatten = require('flat');
+
 const { operatorMap, numberOperators } = require("./constants");
 const { convertGraphQLToFQL, getBaseQuery } = require("./graphqlToFQLConverter");
 const { capitalizeFirstLetter, removeQuotes, checkBool, getByValue } = require("./helper");
 
-let ruleMap = new Map();
+let objectMap = new Map();
 const factMap = new Map();
+let optimizedFactMap = new Map();
 const conditionMap = new Map();
+
+const topLevel = new Map();
+const midLevel = new Map();
+const lowerLevel = new Map();
+
+const functionCall = ' => ';
+
+
+const fMap = new Map();
+const anyMap = new Map();
+const allMap = new Map();
 
 const requestBody = {
   "type": "Rule",
-  "name": "isEligableForCredit",
+  "name": "isEligibleForCredit",
   "all": [
     {
       "type": "Condition",
@@ -111,6 +125,10 @@ const getQueries = (data, queries) => {
   }
 }
 
+// const optimize = (simpleRule) => {
+//   return JSON.stringify(simpleRule.split('&&'))
+// }
+
 /* Optimizing performance and cost of rule by finding duplicated code */
 const optimizeRule = (data, simpleRule) => {
   const { all, any } = data;
@@ -124,7 +142,6 @@ const optimizeRule = (data, simpleRule) => {
 
   for (const query of queries) {
     const udfString = getBaseQuery(query);
-
     if (!map.has(udfString)) {
       map.set(udfString, 1);
     }
@@ -132,6 +149,7 @@ const optimizeRule = (data, simpleRule) => {
       varsString += varsString === ''
         ? `let var${index} = ${udfString}`
         : `\nlet var${index} = ${udfString}`;
+
       simpleRule = simpleRule.replaceAll(udfString, `var${index}`);
       map.set(udfString, map.get(udfString) + 1);
 
@@ -143,8 +161,9 @@ const optimizeRule = (data, simpleRule) => {
       searchParam = capitalizeFirstLetter(searchParam[0]);
 
       let variableName = `${udfSplit[0]}By${searchParam}`;
+      variableName = variableName.replace(/ /g, '');
 
-      ruleMap.set(variableName, udfString);
+      objectMap.set(variableName, udfString);
 
       index++;
     }
@@ -157,20 +176,44 @@ const optimizeRule = (data, simpleRule) => {
 }
 
 const buildRule = () => {
-  const { type } = requestBody;
+  const { type, all, any } = requestBody;
+  let simpleRule;
+
   switch (type.toLowerCase()) {
     case "rule":
-      const simpleRule = buildCompositeRule(requestBody);
-      const optimizedRule = optimizeRule(requestBody, simpleRule);
+      simpleRule = buildCompositeRule(requestBody);
+      optimizeRule(requestBody, simpleRule);
       break;
     case "condition":
-      const factAndCondition = buildFactAndCondition(requestBody);
+      buildFactAndCondition(requestBody);
       break;
   }
 
-  console.log(ruleMap)
-  console.log(factMap)
-  console.log(conditionMap)
+  objectMap.forEach((value, key) => {
+    const object = {key, value};
+    factMap.forEach((value, key) => {
+      const fact = {key, value};
+      optimizeFact(object, fact);
+    });
+  });
+
+  console.log(objectMap);
+  console.log(optimizedFactMap);
+  console.log(conditionMap);
+
+  // console.log( fMap )
+  // console.log( anyMap )
+  // console.log( allMap )
+
+  // objectMap.forEach((value, key) => {
+  //   console.log(createFunction(key, value))
+  // });
+  // optimizedFactMap.forEach((value, key) => {
+  //   console.log(createFunction(key, value))
+  // });
+  // conditionMap.forEach((value, key) => {
+  //   console.log(createFunction(key, value))
+  // });
 }
 
 const buildFactAndCondition = (data) => {
@@ -185,7 +228,7 @@ const buildFactAndCondition = (data) => {
   const sourceType = capitalizeFirstLetter(factName[factName.length-1]);
 
   factName = `fact${collection}${sourceType}`;
-  const fact = `let ${factName} = ${sourceString}`;
+  const fact = `()${functionCall}${sourceString}`;
 
   const formattedComparator = capitalizeFirstLetter(comparator);
   const formattedTarget = capitalizeFirstLetter(removeQuotes(targetString));
@@ -193,12 +236,12 @@ const buildFactAndCondition = (data) => {
   let conditionName;
 
   if(checkBool(target)) {
-    conditionName = target.value ? `conditionHas${sourceType}` : `conditionHasNo${sourceType}`;
+    conditionName = target.value ? `condition${collection}Has${sourceType}` : `condition${collection}HasNo${sourceType}`;
   } else {
-    conditionName = `condition${sourceType}${formattedComparator}${formattedTarget}`;
+    conditionName = `condition${collection}${sourceType}${formattedComparator}${formattedTarget}`;
   }
 
-  const condition = `let ${conditionName} = ${factName}() ${operatorString} ${targetString}`;
+  const condition = `()${functionCall}${factName}() ${operatorString} ${targetString}`;
 
   factMap.set(factName, fact)
   conditionMap.set(conditionName, condition);
@@ -209,38 +252,51 @@ const buildFactAndCondition = (data) => {
   };
 }
 
-const optimizedFact = () => {
+const optimizeFact = (object, fact) => {
 
-  const udf = 'let factUserNationality = user.all.firstWhere(.id == "ckadqdbhk00go0148zzxh4bbq").nationality' // Try edit me
-  const map = new Map()
-  map.set('userById', 'user.all.firstWhere(.id == "ckadqdbhk00go0148zzxh4bbq")')
+  // split object and fact in key value
+  const factName = fact.key;
+  const factValue = fact.value;
 
-  // 1. step to get search param
+  // create map to check object value
+  const map = new Map();
+  map.set(object.key, object.value)
+
+  // 1. step
+  // extract the search path
+  // remove fact
   let temp;
   let queryString;
 
-  temp = udf.split('.');
+  temp = factValue.split('.');
   const factParam = temp[temp.length-1]
-  console.log('fact Param: ' + factParam)
   temp.pop();
 
   queryString = temp.join(".");
 
-// 2. step
-  temp = queryString.split(' = ');
-  console.log('query String: ' + temp[1])
+  // 2. step
+  // get search param from variable
+  // set variable in fact
+  temp = queryString.split(functionCall);
   const searchParam = temp[1]
   temp.pop();
 
   const searchVariable = getByValue(map, searchParam)
-  console.log(searchVariable)
+  if(searchVariable !== undefined) {
+    // Optimized fact
+    // remove space
+    queryString = `${temp}${functionCall}${searchVariable}().${factParam}`;
 
-  queryString = `${temp} = ${searchVariable}().${factParam}`;
+    optimizedFactMap.set(factName, queryString);
+  }
 
-// Log to console
-  console.log(queryString)
+}
 
-  // return optimizedFact;
+const createFunction = (functionName, functionBody) => {
+  return `Function.create({
+    name: '${functionName}',
+    body: '${functionBody}'
+  })`
 }
 
 const getCorrectOperator = (operator) => {
@@ -251,27 +307,44 @@ const getTargetString = (operatorString, target) => {
   const { type, value } = target;
 
   if (type.toLowerCase() === 'fact')
-    return operatorString == '=='
+    return operatorString === '=='
       ? `${convertGraphQLToFQL(value)}`
       : `(${convertGraphQLToFQL(value)})`;
   else if (type.toLowerCase() === 'string')
-    return operatorString == '==' ? `"${value}"` : `("${value}")`
+    return operatorString === '==' ? `"${value}"` : `("${value}")`
 
   return value;
 }
+
 
 const buildNestedConditions = (data, operator) => {
   let ruleString = '';
   for (let index = 0; index < data.length; index++) {
     const { all: allInner, any: anyInner } = data[index];
-    if (allInner)
-      ruleString += `(${buildCompositeRule(data[index])})`;
-    else if (anyInner)
-      ruleString += `(${buildCompositeRule(data[index])})`;
-    else
-      ruleString += `${buildFactAndCondition(data[index])}`;
+    if (allInner) {
+      const allInnerEntry = data[index];
+      ruleString += buildCompositeRule(allInnerEntry);
 
-    if (index != data.length - 1)
+      allMap.set(allInnerEntry.name, allInnerEntry)
+
+      lowerLevel.set('all', allInnerEntry);
+    } else if (anyInner) {
+      const anyInnerEntry = data[index];
+      ruleString += buildCompositeRule(anyInnerEntry);
+
+      anyMap.set(anyInnerEntry.name, anyInnerEntry)
+
+      lowerLevel.set('any', anyInnerEntry);
+    } else {
+      const factInnerEntry = data[index];
+      ruleString += buildFactAndCondition(factInnerEntry);
+
+      fMap.set(factInnerEntry.name, factInnerEntry)
+
+      lowerLevel.set('fact', factInnerEntry);
+    }
+
+    if (index !== data.length - 1)
       ruleString += ` ${operator} `;
   }
   return ruleString;
@@ -279,14 +352,91 @@ const buildNestedConditions = (data, operator) => {
 
 const buildCompositeRule = (data) => {
   const { all, any } = data;
+
   let ruleString = '';
   if (all) {
-    ruleString += `${buildNestedConditions(all, '&&')}`;
+    const allEntry = buildNestedConditions(all, '&&');
+    ruleString += allEntry;
+
+    topLevel.set('all', allEntry)
+  } else if (any) {
+    const anyEntry = buildNestedConditions(any, '||');
+    ruleString += anyEntry;
+
+    topLevel.set('any', anyEntry);
   }
-  else if (any) {
-    ruleString += `${buildNestedConditions(any, '||')}`;
-  }
-  return `${ruleString}`;
+
+  return ruleString;
 }
 
-optimizedFact();
+
+function flattenJSON(obj){
+
+  if (typeof obj !== "object" || obj === null) {
+    return 0;
+  }
+
+  const flat = flatten(obj);
+
+  // console.log(flat)
+
+  const map = new Map(Object.entries(flat));
+  iterateMap(map)
+
+  console.log(topLevel)
+
+  // const ruleName = `${flat.type}${capitalizeFirstLetter(flat.name)}`
+  // const map = new Map();
+
+}
+
+function isNumeric(value) {
+  return /^\d+$/.test(value);
+}
+
+const iterateMap = (data) => {
+
+  let tempMidMap = new Map();
+  let tempLowerMap = new Map();
+
+  let oldName = Array.from(data.keys())[2].split('.');
+  oldName.pop();
+  oldName = oldName.join('.');
+
+  for (const [key, value] of data.entries()) {
+    const keys = key.split('.');
+    keys.pop();
+
+    const updatedAmount = keys.length;
+    const updatedKey = keys[updatedAmount-1];
+    const updatedName = keys.join('.');
+    const checkName = key.slice(0, updatedName.length)
+    const valueName = key.slice(updatedName.length + 1, key.length)
+
+    if(updatedKey !== undefined) {
+      if(isNumeric(updatedKey)) {
+        if(checkName === updatedName) {
+          topLevel.set(updatedName, tempMidMap);
+        }
+      } else {
+        if(checkName === updatedName) {
+          if(oldName !== updatedName) {
+            tempLowerMap = new Map([[valueName, value]]);
+            tempMidMap.set(updatedName, tempLowerMap);
+          } else {
+            tempLowerMap.set(valueName, value);
+            tempMidMap = new Map([[updatedName, tempLowerMap]]);
+          }
+        }
+      }
+    }
+    // necessary for usage of old/new map
+    oldName = updatedName !== '' || updatedName !== undefined ? updatedName : oldName;
+  }
+
+}
+
+flattenJSON(requestBody)
+
+// buildRule();
+
